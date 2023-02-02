f42a08439920b0e9a68a3f99158a3f2566f2312b
KahYengP <s10245147@connect.np.edu.sg>
2023-02-03 00:33:15 +0800

    Update homepage, about us, style css

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+const sr = ScrollReaveal({
+    origin: 'top',
+    distance: '120px',
+    duration: 2500,
+    delay:400,
+    reset: true
+
+})
+
+sr.ScrollReaveal('.container.promotion',{origin: 'left'})
+sr.ScrollReaveal('.home-img',{origin: "right"})
