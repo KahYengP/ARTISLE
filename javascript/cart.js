308960cc665f3a55f2398a41124cf4c350a015c8
KahYengP <s10245147@connect.np.edu.sg>
2023-02-07 20:57:59 +0800

    Update Shop page and cart page

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,85 +1,116 @@
-const payAmountBtn = document.querySelector('#payAmount');
-const decrementBtn = document.querySelectorAll('#decrement');
-const quantityElem = document.querySelectorAll('#quantity');
-const incrementBtn = document.querySelectorAll('#increment');
-const priceElem = document.querySelectorAll('#price');
-const subtotalElem = document.querySelector('#subtotal');
-const taxElem = document.querySelector('#tax');
-const totalElem = document.querySelector('#total');
+$(document).ready(function () {
+    //loading screen
+  $("#summary-products").html("loading...");
 
+  //get item from local storage
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-// loop: for add event on multiple `increment` & `decrement` button
-for (let i = 0; i < incrementBtn.length; i++) {
-
-  incrementBtn[i].addEventListener('click', function () {
-
-    // collect the value of `quantity` textContent,
-    // based on clicked `increment` button sibling.
-    let increment = Number(this.previousElementSibling.textContent);
-
-    // plus `increment` variable value by 1
-    increment++;
-
-    // show the `increment` variable value on `quantity` element
-    // based on clicked `increment` button sibling.
-    this.previousElementSibling.textContent = increment;
-
-    totalCalc();
-
-  });
-
-
-  decrementBtn[i].addEventListener('click', function () {
-
-    // collect the value of `quantity` textContent,
-    // based on clicked `decrement` button sibling.
-    let decrement = Number(this.nextElementSibling.textContent);
-
-    // minus `decrement` variable value by 1 based on condition
-    decrement <= 1 ? 1 : decrement--;
-
-    // show the `decrement` variable value on `quantity` element
-    // based on clicked `decrement` button sibling.
-    this.nextElementSibling.textContent = decrement;
-
-    totalCalc();
-
-  });
-
-}
-
-
-
-// function: for calculating total amount of product price
-const totalCalc = function () {
-
-  // declare all initial variable
-  const tax = 0.05;
-  let subtotal = 0;
-  let totalTax = 0;
-  let total = 0;
-
-  // loop: for calculating `subtotal` value from every single product
-  for (let i = 0; i < quantityElem.length; i++) {
-
-    subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
-
+  if (cart.length === 0) {
+    $("#summary-products").html("No items in cart");
+  } else {
+    $("#summary-products").html("");
+    cart.forEach((item) => {
+      $("#summary-products").append(`
+        <div class="product-card">
+          <div class="card">
+            <div class="img-box">
+              <img src="${item.thumbnail}" alt="Green tomatoes" width="80px" class="product-img">
+            </div>
+            <div class="detail">
+              <h4 class="product-name">${item.name}</h4>
+              <div class="wrapper">
+                <div class="product-qty">
+                  <button id="decrement" data-id="${item.id}">
+                    <ion-icon name="remove-outline"></ion-icon>
+                  </button>
+                  <span id="quantity">${item.quantity}</span>
+                  <button id="increment" data-id="${item.id}">
+                    <ion-icon name="add-outline"></ion-icon>
+                  </button>
+                </div>
+                <div class="price">
+                  $<span id="price">${item.price}</span>
+                </div>
+              </div>
+            </div>
+            <button class="product-close-btn" data-id="${item.id}">
+              <ion-icon name="close-outline"></ion-icon>
+            </button>
+          </div>
+        </div>
+      `);
+    });
   }
 
-  // show the `subtotal` variable value on `subtotalElem` element
-  subtotalElem.textContent = subtotal.toFixed(2);
+   // function: for calculating total amount of product price
+   function totalCalc() {
+    // declare all initial variable
+    const tax = 0.05;
+    let subtotal = 0;
+    let totalTax = 0;
+    let total = 0;
+  
+    // loop: for calculating `subtotal` value from every single product
+    for (let i = 0; i < cart.length; i++) {
+      subtotal += Number(cart[i].quantity) * Number(cart[i].price);
+    }
+  
+    // show the `subtotal` variable value on `subtotalElem` element
+    $("#subtotal").text(subtotal.toFixed(2));
+  
+    // calculating the `totalTax`
+    totalTax = subtotal * tax;
+  
+    // show the `totalTax` on `taxElem` element
+    $("#tax").text(totalTax.toFixed(2));
+  
+    // calcualting the `total`
+    total = subtotal + totalTax;
+  
+    // show the `total` variable value on `totalElem` & `payAmountBtn` element
+    $("#total").text(total.toFixed(2));
+    $("#total-in-button").text(`$${total.toFixed(2)}`);
+  }
 
-  // calculating the `totalTax`
-  totalTax = subtotal * tax;
+  totalCalc();
 
-  // show the `totalTax` on `taxElem` element
-  taxElem.textContent = totalTax.toFixed(2);
+  $("#summary-products").on("click", ".product-close-btn", function () {
+    const id = $(this).data("id");
+    const newCart = cart.filter((item) => item.id !== id);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    location.reload();
+  });
 
-  // calcualting the `total`
-  total = subtotal + totalTax;
+  $("#summary-products").on("click", "#increment", function () {
+    const id = $(this).data("id");
+    const newCart = cart.map((item) => {
+      if (item.id === id) {
+        item.quantity++;
+      }
+      return item;
+    });
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    location.reload();
+  });
 
-  // show the `total` variable value on `totalElem` & `payAmountBtn` element
-  totalElem.textContent = total.toFixed(2);
-  payAmountBtn.textContent = total.toFixed(2);
+  $("#summary-products").on("click", "#decrement", function () {
 
-}
+    const id = $(this).data("id");
+    const newCart = cart.map((item) => {
+      if (item.id === id) {
+        item.quantity--;
+      }
+      return item;
+    });
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    location.reload();
+  });
+
+  $("#checkout-btn").click(function () {
+    alert("Thank you for your purchase!");
+    // API to save cart to airtable to do
+    localStorage.removeItem("cart");
+    location.reload();
+  });
+});
+
--- javascript/shop.js
+++ javascript/shop.js
@@ -1,52 +1,100 @@
 const AIRTABLE_API_KEY = "patRgUH9Rcn6kS52E.bbed58defa5c76d075bc6052ebb1dc7389fff8f290360716d868ae1fd1c53d4b";
 
-const products = axios.create({
-  baseURL: 'https://api.airtable.com/v0/appgzJKZmNGJoa4IH/products/recq5mRkoc0LXGpLC',
+const axiosInstance = axios.create({
+  baseURL: 'https://api.airtable.com/v0/appgzJKZmNGJoa4IH',
   headers: {
     'Authorization': `Bearer ${AIRTABLE_API_KEY}`
   }
 });
 
-function getItem(){
-    $("#item-list").html(`
-    <div class="spinner-border text-primary" role="status"></div>
-    `);
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+$(document).ready(function () {
+    function getItem(){
+        // loading screen
+        $("#product-list").html(`
+            <div class="text-center">
+                Loading...
+            </div>
+        `);
 
-    const searchKeyword = urlParams.get('searchKeyword');
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
 
-    console.log(searchKeyword)
+        const searchKeyword = urlParams.get('searchKeyword');
 
-    const query = searchKeyword ? `?filterByFormula=SEARCH('${searchKeyword}'%2C+%7Bname%7D)` : "";
+        console.log("current search keywords", searchKeyword)
 
+        const query = searchKeyword ? `?filterByFormula=SEARCH('${searchKeyword}'%2C+%7Bname%7D)` : "";
 
-    axiosInstance.get(`items${query}`).then((response) => {
-        const items = response.data.records;
-        console.log(items)
-        let formattedItems = "";
+        console.log("calling air table")
+        axiosInstance.get(`/products${query}`).then((response) => {
+            console.log("response from airtable", response);
+            const items = response.data.records;
 
-        for (let i = 0; i < items.length; i++) {
-            const item = items[i];
-    
-            formattedItems += `
-                <div class="product text-center col-lg-3 col-md-4 col-12">
-                    <img class="img-fluid mb-3" src="products/oil3-fc-pastels.jpg" alt="">
-                    <h5 class="p-name">Faber Castell Oil Pastels 12 Colours</h5>
-                    <h4 class="p-price"> $4.00</h4>
-                    <button class="buy-btn">Buy Now</button>
+            let formattedItems = "";
+
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+
+                console.log(item)
+        
+                formattedItems += `
+                    <div class="product text-center col-lg-3 col-md-4 col-12">
+                        <img class="img-fluid mb-3" src="${item.fields.Image[0].url}" alt="">
+                        <h5 class="p-name">${item.fields.Name}</h5>
+                        <h4 class="p-price">$${item.fields.Price.toFixed(2)}</h4>
+                        <button 
+                            class="add-btn" 
+                            data-id="${item.id}"
+                            data-price="${item.fields.Price}"
+                            data-name="${item.fields.Name}"
+                            data-thumbnail="${item.fields.Image[0].url}"
+                        >+Add to Cart</button>
+                        </div>
                     </div>
-                </div>
-            `
+                `
+            }
+            //display after the loading screen
+            $("#product-list").html(formattedItems);
+        });
+    }
+
+    getItem();
+
+    function addItemToCart(item) {
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        const itemInCart = cart.find(i => i.id === item.id);
+
+        if (itemInCart) {
+            itemInCart.quantity++;
+            alert("Item quantity updated!");
+        } else {
+            cart.push({
+                ...item,
+                quantity: 1
+            });
+            alert("Item added to cart!");
         }
-        $("#item-list").html(formattedItems);
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+
+    }
+
+    $("#product-list").on("click", ".add-btn", function (event) {
+        event.preventDefault();
+
+        const itemId = $(this).data("id");
+        const price = $(this).data("price");
+        const name = $(this).data("name");
+        const thumbnail = $(this).data("thumbnail");
+
+        const item = {
+            id: itemId,
+            price: price,
+            name: name,
+            thumbnail: thumbnail
+        };
+
+        addItemToCart(item);
     });
-
-
-
-
-
-
-
-}
+});
